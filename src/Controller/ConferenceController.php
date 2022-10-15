16464fb9c3d7baa5f0d95c9248cb5be336cbea68
bondarval <bondamaker@gmail.com>
2022-10-15 21:35:51 +0500

    Mail and browser notifications added

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -14,6 +14,8 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -65,7 +67,7 @@ class ConferenceController extends AbstractController
      * @throws LoaderError
      */
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, NotifierInterface $notifier, string $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -82,19 +84,25 @@ class ConferenceController extends AbstractController
                     $comment->setPhotoFilename($filename);
             }
 
-                $this->entityManager->persist($comment);
-                $this->entityManager->flush();
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
 
-                $context = [
-                    'user_ip' => $request->getClientIp(),
-                    'user_agent' => $request->headers->get('user-agent'),
-                    'referrer' => $request->headers->get('referer'),
-                    'permalink' => $request->getUri(),
-                ];
-                $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
-                return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
-            }
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
+        if ($form->isSubmitted()) {
+            $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
+        }
 
         $offset = max(0, $request->query->getInt('offset'));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
